83e4dda6c064eda1fc0eba2582705e7e853251b4
Tryd0g0lik <work80@mail.ru>
2023-05-06 06:31:32 +0700

    task complated

--- src/js/app.js
+++ src/js/app.js
@@ -1,47 +1,38 @@
-import { default as read } from './reader.js';
-import { default as json } from './parser.js';
+import { default as read } from './reader';
+import { default as json } from './parser';
 
 
 export default class GameSavingLoader {
   load() {
-    // const response =
     return (async () => {
-      // try {
       const resp = await read();
-      const respone = await json(resp);
+      const ans = await json(resp);
+      const answer = JSON.parse(ans);
+      class GameSaving {
+        constructor() {
+          this.ind = answer.id;
+          this.createds = answer.created;
+          this.userInfos = {
+            'id': answer.userInfo.id,
+            'name': answer.userInfo.name,
+            'level': answer.userInfo.level,
+            'points': answer.userInfo.points,
+          };
+        }
+      }
+      const respone = new GameSaving();
       return respone;
-      // } catch (e) {
-      //   throw (e.message);
-      // }
     })();
-    // return response;
   }
 }
 
 
-function pushung() {
-  const response = new GameSavingLoader();
-  return (async () => {
-    class GameSaving {
-      constructor(answer) {
-        this.ind = answer.id;
-        this.createds = answer.created;
-        this.userInfos = {
-          "id": answer.userInfo.id,
-          "name": answer.userInfo.name,
-          "Hitman": answer.userInfo.Hitman,
-          "level": answer.userInfo.level,
-          "points": answer.userInfo.points,
-        };
-      }
-    };
+// function pushung() {
+//   return (async () => {
+//     const response = await new GameSavingLoader();
+//     const i = await response.load();
+//     console.log(i.ind);
 
-    const ans = await response.load();
-    const newAns = JSON.parse(ans);
-    const objClassGameSaving = new GameSaving(newAns);
-    console.log("=======>>", objClassGameSaving)
-
-    return objClassGameSaving
-  })();
-}
-pushung();
+//   })();
+// }
+// pushung();
--- webpack.config.js
+++ webpack.config.js
@@ -1,70 +1,65 @@
 // Generated using webpack-cli https://github.com/webpack/webpack-cli
 
 const path = require("path");
+require("request");
 const EslintPlugin = require("eslint-webpack-plugin");
-const HtmlWebpackPlugin = require("html-webpack-plugin");
-var request = require('request');
-var webpack = require('webpack');
+const webpack = require("webpack");
+const ESLintWebpackPlugin = require("eslint-webpack-plugin");
+import('jest').Config
 // const isProduction = process.env.NODE_ENV == "production";
 
 module.exports = {
-	entry: "./src/index.js",
-	output: {
-		path: path.resolve(__dirname, "dist"),
-		filename: "[name].js",
-		publicPath: "js/",
-		libraryTarget: "this",
-	},
-	devServer: {
-		open: true,
-		host: "localhost",
-	},
-	plugins: [
-		new EslintPlugin({
+  entry: "./src/index.js",
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "[name].js",
+    compareBeforeEmit: true, // проверяет существует ли файл и его содержимое 
+    publicPath: "./js/",
+
+    libraryTarget: "this",
+  },
+  devServer: {
+    open: true,
+    host: "localhost",
+  },
+  plugins: [
+    new ESLintWebpackPlugin({
       overrideConfigFile: path.resolve(__dirname, ".eslintrc.js"),
-			files: path.resolve(__dirname, "./src/js/"),
-		}),
-    new HtmlWebpackPlugin({
-			minify: {
-				collapseWhitespace: false,
-      }
+      files: path.resolve(__dirname, "./src/js/")
     }),
     new webpack.LoaderOptionsPlugin({
       options: {
-        minimize: true,
-
+        minimize: false,
       }
-    })
-		// Add your plugins here
-		// Learn more about plugins from https://webpack.js.org/configuration/plugins/
-	],
-	module: {
-		rules: [
-			{
-				test: /\.(js|jsx)?$/,
-				exclude: /node_modules/,
-				use: {
-					loader: "babel-loader",
-				},
+    }),
+    // Add your plugins here
+    // Learn more about plugins from https://webpack.js.org/configuration/plugins/
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/i,
+        exclude: "/node_modules/",
+        use: {
+          loader: "babel-loader",
+        }
       },
       {
         test: /\.(eot|svg|ttf|woff|woff2|png|jpg|gif)$/i,
         type: "asset",
       },
-			// Add your rules for custom modules here
-			// Learn more about loaders from https://webpack.js.org/loaders/
-		],
-	},
-	resolve: {
-		extensions: ['.js', '.jsx'],
-		modules: [
-      "node_modules",
-      'bower_components',
-		]
+
+      // Add your rules for custom modules here
+      // Learn more about loaders from https://webpack.js.org/loaders/
+    ],
   },
-  stats: {
-    children: true
-  }
+  resolve: {
+    extensions: ['.js', '.jsx'],
+    modules: [
+      'node_modules',
+      'bower_components',
+    ],
+  },
 };
 
 // module.exports = () => {
